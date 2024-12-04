0814d4d1b5fc2be5317b56e78e6293397f9c3f09
vallit0 <estuardovalleb@gmail.com>
2024-12-04 11:55:43 -0600

    sidebar medio agregada

--- chat-app/src/App.jsx
+++ chat-app/src/App.jsx
@@ -1,18 +1,25 @@
 import React, { useState } from "react";
 import WelcomeScreen from "./components/WelcomeScreen";
 import Chat from "./components/Chat";
+import Sidebar from "./components/Sidebar";
 import "./App.css";
 
 const App = () => {
-  const [showWelcome, setShowWelcome] = useState(true); // Estado para controlar la pantalla de bienvenida
+  const [showWelcome, setShowWelcome] = useState(true); // Pantalla de bienvenida
+  const [isSidebarVisible, setSidebarVisible] = useState(true); // Visibilidad de la barra lateral
 
   const handleSend = () => {
-    setShowWelcome(false); // Oculta la bienvenida al enviar un mensaje
+    setShowWelcome(false); // Oculta la bienvenida
+  };
+
+  const toggleSidebar = () => {
+    setSidebarVisible(!isSidebarVisible); // Alterna la visibilidad de la barra lateral
   };
 
   return (
     <div className={`app ${showWelcome ? "welcome-active" : "chat-active"}`}>
       {showWelcome && <WelcomeScreen />}
+      {isSidebarVisible && <Sidebar onToggleSidebar={toggleSidebar} />}
       <Chat onSend={handleSend} />
     </div>
   );
--- chat-app/src/components/Sidebar.jsx
+++ chat-app/src/components/Sidebar.jsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React from "react";
+import "./Sidebar.css";
+import Dropdown from "./Dropdown";
 
-const Sidebar = () => {
-  const chats = ['the first chat', 'second chat', 'let’s see', 'test mutation'];
+const Sidebar = ({ onToggleSidebar }) => {
+  const chats = ["the first chat", "second chat", "let’s see", "test mutation"];
 
   return (
     <div className="sidebar">
-      <h2>Recent Chats</h2>
+      <div className="sidebar-header">
+        
+        <div className="sidebar-title">
+          <img
+            src="https://img.icons8.com/fluency/48/000000/lightning-bolt.png"
+            alt="Logo"
+            className="sidebar-logo"
+          />
+          <h2>Spike 1.0</h2>
+          <button className="sidebar-toggle-button" onClick={onToggleSidebar}>
+          ✖
+        </button>
+        </div>
+      </div>
+      
+      <Dropdown />
+      <h3>Chats</h3>
       <ul>
         {chats.map((chat, index) => (
           <li key={index}>{chat}</li>
